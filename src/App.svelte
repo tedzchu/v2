560731c3ba7ac09b540c1b3795f345a0aa7a5903
Teddy Chu <tedchu4@gmail.com>
2021-10-07 00:49:38 -0700

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,75 @@
+<script lang="ts">
+  import Icon from "./components/Icon.svelte";
+</script>
+
+<main>
+  <h1>Teddy Chu</h1>
+  <p>
+    Yo! 👋 I'm <strong>Teddy</strong>, a Los Angeles-based
+    <strong>software engineer</strong>. I sometimes respond to <strong>Door</strong>, as it's the funnier (and probably not real) nickname for my full name, Theodore.
+  </p>
+  <p>
+    I'm currently a <strong>full stack</strong> engineer at
+    <strong><a href="https://www.atlassian.com/">Atlassian</a></strong>
+    building solutions to expand their
+    <strong><a href="https://www.atlassian.com/platform">platform</a></strong> more
+    efficiently.
+  </p>
+  <p>
+    I have industry experience with <strong>Java</strong>,
+    <strong>Python</strong>, and <strong>Kotlin</strong> on the backend and
+    <strong>React</strong>
+    with <strong>TypeScript</strong> on the frontend.
+  </p>
+  <p>
+    Some hobby hats I wear include being a <strong>cook</strong> and
+    <strong>foodie</strong>, <strong>weightlifter</strong>, and world-class
+    <strong>video game speedrunner</strong>.
+  </p>
+  <p>
+    If you're interested in learning more about me, you can check me out below:
+  </p>
+  <Icon name="github" url="https://github.com/tedzchu/" />
+  <Icon name="linkedin" url="https://www.linkedin.com/in/teddy-chu/" />
+  <Icon name="twitter" url="https://twitter.com/doorDoes" />
+  <Icon
+    name="youtube"
+    url="https://www.youtube.com/channel/UCoq7axWlzAOqNnCSecnOXRA"
+  />
+  <br />
+  <small>
+    (My Twitter and Youtube are more gaming-oriented, and mostly inactive)
+  </small>
+  <br />
+  <br />
+  <p>
+    Looking for more? Here's my <strong><a href="/docs/TeddyChu_Resume_10_2021.pdf">resume</a></strong>, which
+    has my email if you're looking to get in touch. Cheers!
+  </p>
+</main>
+
+<style>
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 350px;
+    margin: 0 auto;
+  }
+
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 3em;
+    font-weight: 100;
+  }
+
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+
+    h1 {
+      font-size: 4em;
+    }
+  }
+</style>
