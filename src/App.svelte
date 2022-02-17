7f9eca2f2cdcc629a7235463e954ea5b09658554
Teddy Chu <tedchu4@gmail.com>
2022-02-16 21:15:14 -0800

    Update resume

--- src/App.svelte
+++ src/App.svelte
@@ -7,45 +7,41 @@
   <p>
     Yo! 👋 I'm <strong>Teddy</strong>, a Los Angeles-based
     <strong>software engineer</strong>. I sometimes respond to
-    <strong>Door</strong>, as it's the funnier (and probably not real) nickname
-    for my full name, Theodore.
+    <strong>Door</strong> 🚪, as it's the funnier "nickname" for my full name, Theodore.
   </p>
   <p>
     I'm currently a <strong>full stack</strong> engineer at
     <strong><a href="https://www.atlassian.com/">Atlassian</a></strong>
-    building solutions to reduce friction for products trying to adopt their
+    building solutions to reduce friction for products trying to adopt Atlassian's
     <strong><a href="https://www.atlassian.com/platform">platform</a></strong> capabilities.
   </p>
   <p>
-    I have industry experience with <strong>Java</strong>,
-    <strong>Python</strong>, and <strong>Kotlin</strong> on the backend and
+    I have industry experience writing <strong>Java</strong>,
+    <strong>Python</strong>, and <strong>Kotlin</strong> backends and
     <strong>React</strong>
-    with <strong>TypeScript</strong> on the frontend.
+    with <strong>TypeScript</strong> frontends. If I'm lucky, I sometimes get to
+    write
+    <strong>Terraform</strong>.
   </p>
   <p>
-    Some hobby hats I wear include being a <strong>cook</strong> and
-    <strong>foodie</strong>, <strong>weightlifter</strong>, and world-class
+    I'm a budding <strong>functional programming</strong> enthusiast, trying to
+    get more experience with <strong>Haskell</strong> in my spare time.
+  </p>
+  <p>
+    Some hobby hats I wear include being a <strong>cook</strong>,
+    <strong>videographer</strong>, <strong>powerlifter</strong>, and world-class
     <strong>video game speedrunner</strong>.
   </p>
   <p>
-    If you're interested in learning more about me, you can check me out below:
+    If you're interested in learning more about me, you can check me out here:
   </p>
   <Icon name="github" url="https://github.com/tedzchu/" />
   <Icon name="linkedin" url="https://www.linkedin.com/in/teddy-chu/" />
-  <Icon name="twitter" url="https://twitter.com/doorDoes" />
-  <Icon
-    name="youtube"
-    url="https://www.youtube.com/channel/UCoq7axWlzAOqNnCSecnOXRA"
-  />
-  <br />
-  <small>
-    (My Twitter and YouTube are more gaming-oriented, and mostly inactive)
-  </small>
   <br />
   <br />
   <p>
     Looking for more? Here's my <strong
-      ><a href="/docs/TeddyChu_Resume_10_2021.pdf">resume</a></strong
+      ><a href="/docs/TeddyChu_Resume_02_2022.pdf">resume</a></strong
     >, which has my email if you're looking to get in touch. Cheers!
   </p>
 </main>
